08c4676615047dc9044dbe0c00e2993cfdafa4f0
Stef Kariotidis <stef.kariotidis@gmail.com>
2026-03-15 12:50:45 +0200

    Refactor ops and host-local services out of main

---
+++ app/ops/__init__.py
@@ -0,0 +1,8 @@
+from .service import ops_catalog_service, ops_run_service, ops_schedule_export_service, ops_status_service
+
+__all__ = [
+    "ops_catalog_service",
+    "ops_run_service",
+    "ops_schedule_export_service",
+    "ops_status_service",
+]
