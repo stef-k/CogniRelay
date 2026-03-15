e1577b6550f85ce971680e025cec9275e61d7ece
Stef Kariotidis <stef.kariotidis@gmail.com>
2026-03-15 16:56:09 +0200

    Refactor security services out of main

---
+++ app/security/__init__.py
@@ -0,0 +1,33 @@
+from .service import (
+    GOVERNANCE_POLICY_REL,
+    NONCE_INDEX_REL,
+    SECURITY_KEYS_REL,
+    TOKEN_CONFIG_REL,
+    governance_policy_service,
+    load_token_config,
+    load_security_keys,
+    messages_verify_service,
+    security_keys_rotate_service,
+    security_tokens_issue_service,
+    security_tokens_list_service,
+    security_tokens_revoke_service,
+    security_tokens_rotate_service,
+    verify_signed_payload_service,
+)
+
+__all__ = [
+    "GOVERNANCE_POLICY_REL",
+    "NONCE_INDEX_REL",
+    "SECURITY_KEYS_REL",
+    "TOKEN_CONFIG_REL",
+    "governance_policy_service",
+    "load_token_config",
+    "load_security_keys",
+    "messages_verify_service",
+    "security_keys_rotate_service",
+    "security_tokens_issue_service",
+    "security_tokens_list_service",
+    "security_tokens_revoke_service",
+    "security_tokens_rotate_service",
+    "verify_signed_payload_service",
+]
