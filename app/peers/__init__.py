c6c4ac0b6d80178b8173673567336ae5521f9168
Stef Kariotidis <stef.kariotidis@gmail.com>
2026-03-15 12:59:54 +0200

    Refactor peers services out of main

---
+++ app/peers/__init__.py
@@ -0,0 +1,17 @@
+from .service import (
+    PEERS_REGISTRY_REL,
+    load_peers_registry,
+    peer_manifest_service,
+    peers_list_service,
+    peers_register_service,
+    peers_trust_transition_service,
+)
+
+__all__ = [
+    "PEERS_REGISTRY_REL",
+    "load_peers_registry",
+    "peer_manifest_service",
+    "peers_list_service",
+    "peers_register_service",
+    "peers_trust_transition_service",
+]
